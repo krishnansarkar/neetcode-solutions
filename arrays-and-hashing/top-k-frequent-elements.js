64f14a74ee60f6fb0a48eeab3199001997f94364
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-03-12 22:16:08 -0400

    feat: add solution for 'Top K Frequent Elements'

---
+++ arrays-and-hashing/top-k-frequent-elements.js
@@ -0,0 +1,54 @@
+// Top K Frequent Elements
+
+// Given an integer array nums and an integer k, return the k most frequent elements within the array.
+// The test cases are generated such that the answer is always unique.
+// You may return the output in any order.
+
+// Example 1:
+// Input: nums = [1,2,2,3,3,3], k = 2
+// Output: [2,3]
+
+// Example 2:
+// Input: nums = [7,7], k = 1
+// Output: [7]
+
+// Constraints:
+// 1 <= nums.length <= 10^4.
+// -1000 <= nums[i] <= 1000
+// 1 <= k <= number of distinct elements in nums.
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        var numCounts = new Map();
+
+        for (const num of nums) {
+            if (numCounts.has(num)) numCounts.set(num, numCounts.get(num) + 1);
+            else numCounts.set(num, 1);
+        }
+
+        var numCountsArray = [];
+        for (const [key, value] of numCounts) {
+            numCountsArray.push([key, value]);
+        }
+        numCountsArray.sort((a, b) => {
+            if (a[1] > b[1]) return -1;
+            if (a[1] < b[1]) return 1;
+            return 0;
+        });
+
+        var result = [];
+        for (let i = 0; i < k; i++) {
+            result.push(numCountsArray[i][0]);
+        }
+
+        return result;
+    }
+}
+
+// Time-complexity: O(n log n)
+// Space-complexity: O(n)
