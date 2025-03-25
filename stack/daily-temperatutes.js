397659693ed3b84b6d9a53a725eec856b03301c6
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-03-24 22:35:41 -0400

    feat: add solution for 'Daily Temperatures'

---
+++ stack/daily-temperatutes.js
@@ -0,0 +1,45 @@
+// Daily Temperatures
+
+// You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+// Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+
+// Example 1:
+// Input: temperatures = [30,38,30,36,35,40,28]
+// Output: [1,4,1,2,1,0,0]
+
+// Example 2:
+// Input: temperatures = [22,21,20]
+// Output: [0,0,0]
+
+// Constraints:
+// 1 <= temperatures.length <= 1000.
+// 1 <= temperatures[i] <= 100
+
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        var result = new Array(temperatures.length).fill(0);
+
+        for (let i = 0; i < temperatures.length - 1; i++) {
+            let startTemp = temperatures[i];
+            for (let ii = i + 1; ii < temperatures.length; ii++) {
+                let endTemp = temperatures[ii];
+                if (endTemp > startTemp) {
+                    result[i] = ii - i;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+// var solution = new Solution();
+// console.log(solution.dailyTemperatures([30, 38, 30, 36, 35, 40, 28]));
+
+// Time-complexity: O(n^2)
+// Space-complexity: O(n)
